c975a351f70a270dacdded1e35ec56ddeb01c9f2
siyual-park <kdpark0723@gmail.com>
2021-05-30 14:21:52 +0900

    Add ass 6

---
+++ assignments_6/20180284.c
@@ -0,0 +1,72 @@
+#define BUFFER_SIZE 4096
+#define LINE_SIZE 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "20180284.h"
+
+// An example of a semphore for the mutual exclusion
+semaphore_t mutex;
+FILE *file;
+
+char buffer[BUFFER_SIZE] = { 0 };
+int allocated_size = 0;
+
+void reader(void)
+{
+    P(&mutex);
+    if (feof(file)) {
+        V(&mutex);
+        return;
+    }
+
+    char line[LINE_SIZE] = { 0 };
+    if (fgets(line, LINE_SIZE, file) == NULL) {
+        V(&mutex);
+        return;
+    }
+    int line_size = strlen(line);
+    memcpy(buffer + allocated_size, line, line_size);
+
+    V(&mutex);
+    reader();
+}
+
+void writer(void)
+{
+    P(&mutex);
+    if (allocated_size == 0) {
+        V(&mutex);
+        return;
+    }
+
+    printf("%s", buffer);
+    memset(buffer, 0, BUFFER_SIZE);
+
+    V(&mutex);
+    writer();
+}
+
+int main(int argc, char *argv[])
+{
+	// example of initializing a semaphore
+    mutex = sem_create(1);
+
+    file = fopen(argv[1], "r");
+
+    // create threads
+    create_thread(reader);
+    create_thread(reader);
+    create_thread(reader);
+    create_thread(writer);
+    
+    // Let's run
+    run();
+
+    fclose(file);
+}   
+
+
+
+
--- assignments_6/20180284.h
+++ assignments_6/20180284.h
@@ -5,35 +5,62 @@
 
 #define STACK_SIZE 8192
 
+typedef struct tcb_t {
+    struct tcb_t *next;
+    struct tcb_t *prev;
+    ucontext_t   context;
+    int threadid;
+}tcb_t;
+
 tcb_t *thread_queue = 0;
 tcb_t *curr_thread = 0;
 int g_tid = 1;
 
 // addQueue(head, item) adds a queue item, pointed to by “item”, to the queue pointed to by head.
 void addQueue(tcb_t **head, tcb_t *item) {
-	// your code
+    if (item == NULL) {
+        return;
+    }
+
+    tcb_t *current = *head;
+    if (current == NULL) {
+        *head = item;
+        return;
+    }
+
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = item;
+    item->prev = current;
 }
 
 // delQueue(head) deletes an item from head and returns a pointer to the deleted item. If the queue is already empty, flag error.
 tcb_t *delQueue(tcb_t **head) {
-	// your code
+    if (head == NULL) {
+        return NULL;
+    }
+
+    tcb_t *fist = *head;
+    if (fist->next == NULL) {
+        *head = NULL;
+        return fist;
+    }
+
+    fist->next->prev = NULL;
+    fist->next = NULL;
+
+    return fist;
 }
 
-typedef struct tcb_t {
-	struct tcb_t *next;
-	struct tcb_t *prev;
-	ucontext_t   context;
-	int threadid;
-}tcb_t;
-
-void init_TCB (tcb_t *tcb, void *function, void *stackP, int stack_size) {
+void init_TCB(tcb_t *tcb, void *function, void *stackP, int stack_size) {
 	memset(tcb,'\0', sizeof(tcb_t));
 	getcontext(&tcb->context);
 	tcb->context.uc_stack.ss_sp = stackP;
 	tcb->context.uc_stack.ss_size = (size_t) stack_size;
 	makecontext(&tcb->context,function,0);
 }
-
 
 void create_thread(void (*function)(void)) {
 	void *stack = (void *)malloc(STACK_SIZE);
@@ -73,14 +100,21 @@ typedef struct semaphore_t {
 
 // mallocs a semaphore structure, initializes it to the value and returns the pointer to the semaphore.
 semaphore_t sem_create(int value) {
-
+    tcb_t *prev_thread = curr_thread;
+    curr_thread = delQueue(&thread_queue);
+    return (semaphore_t) { .counter = value, .sem_q = prev_thread };
 }
 
 // decrements the semaphore. 
 // if the value is <= 0, then blocks the thread in the queue associated with the semaphore.
 void P(semaphore_t *sem) {
 	sem->counter--;
-	// your code
+
+	if (sem->counter <= 0) {
+        sem->counter--;
+        addQueue(&(sem->sem_q), curr_thread);
+        run();
+    }
 }
 
 // increments the semaphore.
@@ -88,6 +122,12 @@ void P(semaphore_t *sem) {
 // Note: The V routine also "yields" to the next runnable thread.
 void V(semaphore_t *sem) {
 	sem->counter++;
-	// your code
+
+    if (sem->counter > 0) {
+        sem->counter--;
+        tcb_t *prev_thread = delQueue(&(sem->sem_q));
+        addQueue(&thread_queue, prev_thread);
+    }
+
 	yield();
 }
